e2151dcd20650ffab4ce3c0b05b20d50525951eb
Jesse Janssen <jesse@jessejanssen.nl>
2026-06-26 19:25:52 +0200

    lay out export as nested per-module htsw projects

--- pyhtsw/container.py
+++ pyhtsw/container.py
@@ -380,7 +380,8 @@ class Container:
         return importables
 
     def export(self, name: str) -> None:
-        from .importable import Project, export_import_json
+        from .importable import Project
+        from .module_export import export_project
 
         importables = self._collect_importables(name)
         if not importables:
@@ -408,7 +409,7 @@ class Container:
         project = Project(root)
         self.project = project
         try:
-            export_import_json(project, importables)
+            export_project(project, importables)
         finally:
             self.project = None
 
